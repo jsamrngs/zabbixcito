6bd00888cd57b381571251ed157bf110ca99ae96
jsamrngs <linkjv29@gmail.com>
2026-06-18 15:24:23 -0400

    Update SMTP configuration for email alerts

--- config/smtp_config.py
+++ config/smtp_config.py
@@ -1,5 +1,6 @@
+#Envio de alertas por correo electronico
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
-SMTP_USER = "reporte@caprat.cl"
-SMTP_PASS = "ppjt cnrm vaqp uuqv"
-DESTINATARIO = "justhin.villagran@caprat.cl"
+SMTP_USER = "reporte@empresa.cl"
+SMTP_PASS = "xxxx xxxx xxxx xxxx" #<-- Tu contrasena de aplicaciones
+DESTINATARIO = "nombre.apellido@empresa.cl"
