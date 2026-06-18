68aa82bd19724d18a711db12c96b2383c6840072
jsamrngs <linkjv29@gmail.com>
2026-06-18 15:19:34 -0400

    Update device.py

--- config/device.py
+++ config/device.py
@@ -1,8 +1,9 @@
+#Ingresa tus dispositivos por mac
 AP = [
-    {"nombre": "Repetidor tp-link-Inspectoria", "mac": "B0BE762DE159"},
-    {"nombre": "Repetidor tp-link-PIE", "mac": "B0BE762DE131"},
-    {"nombre": "Repetidor tp-link-Administracion", "mac": "B0BE762DE128"},
+    {"nombre": "Repetidor tp-link-1", "mac": "B0BE762DE159"},
+    {"nombre": "Repetidor tp-link-2", "mac": "B0BE762DE131"},
+    {"nombre": "Repetidor tp-link-3", "mac": "B0BE762DE128"},
     # Servidores Web sin MAC, se verificará por dominio
-    {"nombre": "Servidor Web caprat.cl", "mac": ""},
-    {"nombre": "Servidor Web napsis.com", "mac": ""}
+    {"nombre": "Servidor Web mipaginaweb.cl", "mac": ""},
+    {"nombre": "Servidor Web miotrapaginaweb.com", "mac": ""}
 ]
